ec043f14e720146583e11082d39ca395b2c19b31
Connor <chorman64@gmail.com>
2021-08-04 20:54:10 -0400

    Update

--- libc/include/setjmp.h
+++ libc/include/setjmp.h
@@ -1,16 +1,35 @@
-//
-// Created by chorm on 2020-02-10.
-//
+// 
+// MIT License
+// 
+// Copyright (c) 2021 SNES-Dev Developers
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
 
 #ifndef SNES_DEV_SETJMP_H
 #define SNES_DEV_SETJMP_H
 
 typedef struct __jmp_buf{
-    __short_ptr(void) __sp;
+    int __sp;
     unsigned char __s;
 } jmp_buf[1];
 
-int __setjmp(jmp_buf __buf)__attribute__((returns_twice));
+int __setjmp(jmp_buf __buf) __attribute__((returns_twice));
 
 void longjmp(jmp_buf __buf,int __ret_val) __attribute__((noreturn));
 
--- libc/include/stddef.h
+++ libc/include/stddef.h
@@ -1,14 +1,33 @@
-//
-// Created by chorm on 2020-01-03.
-//
+// 
+// MIT License
+// 
+// Copyright (c) 2021 SNES-Dev Developers
+// 
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+// 
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+// 
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
 
 #ifndef SNES_DEV_STDDEF_H
 #define SNES_DEV_STDDEF_H
 
 #include <qc_int.h>
 
-typedef _UIntShortPtr size_t;
-typedef _IntShortPtr ptrdiff_t;
+typedef unsigned int size_t;
+typedef signed int ptrdiff_t;
 
 #define NULL 0
 
